ff5e843717ae51e892cb289c48daa8a8738b3c65
Evelina Semezyte <evelina.semezyte@gmail.com>
2022-02-14 13:30:55 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Evelina Semezyte on 2022-02-14.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
