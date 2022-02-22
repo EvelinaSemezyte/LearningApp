8fb4fe4b719c879736e5d1210a62a43e4f0770f0
Evelina Semezyte <evelina.semezyte@gmail.com>
2022-02-22 18:41:16 +0200

    - Add getRemoteData method to the ContentModel

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -36,11 +36,14 @@ class ContentModel: ObservableObject {
     @Published var currentTestSelected: Int?
     
     init() {
+        // Parse local included json data
         getLocalData()
+        
+        // Download remote json and parse data
+        getRemoteData()
     }
     
     //  MARK: - Data Methods
-    
     func getLocalData() {
         
         // Get a url to the json file
@@ -77,6 +80,55 @@ class ContentModel: ObservableObject {
             // Log error
             print("Couldn't parse style data: \(error)")
         }
+    }
+    
+    func getRemoteData() {
+        
+        // String path
+        let urlString = "https://evelinasemezyte.github.io/learningapp-data/data2.json"
+        
+        // Create a url object
+        let url = URL(string: urlString)
+        
+        guard url != nil else {
+            // Couldn't create url
+            return
+        }
+        
+        // Create a URLRequest object
+        let request = URLRequest(url: url!)
+        
+        // Get the session and kick off the task
+        let session = URLSession.shared
+        
+        let dataTask =  session.dataTask(with: request) { data, response, error in
+            
+            // Check if there's an error
+            guard error == nil else {
+                // There was an error
+                return
+            }
+            
+            // Handle the responce
+            do {
+                
+                // Create json decoder
+                let decoder = JSONDecoder()
+                
+                // Decode
+                let modules = try decoder.decode([Module].self, from: data!)
+                
+                // Append parsed modules into modules property
+                self.modules += modules
+                
+            }
+            catch {
+                // Couldn't parse json
+            }
+        }
+        
+        // Kick off data task
+        dataTask.resume()
     }
     
     // MARK: - Module navigation methods
